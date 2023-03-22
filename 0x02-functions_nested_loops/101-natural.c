485d092c84acad3074013dc94feb4f746e91d48c
RichardKungu <rkungu12@gmail.com>
2023-03-22 12:56:43 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,68 +1,24 @@
 #include <stdio.h>
-#include <unistd.h>
 #include "main.h"
 
 /**
- * print_times_table - function declaration
- * Description: print the time table
- * @n: the number to use
- * Return: void
+ * main - entry point
+ * Description: sum numbers if %3 %5 is 0
+ * Return: 0
  */
 
-void print_times_table(int n);
-
-/**
- * print_times_table - function definition
- * Description: print entire times table of specified number
- * @n: number to use
- * Return: void
- */
-
-
-void print_times_table(int n)
+int main(void)
 {
-	int row;
-	int column;
+	int test_num;
+	int sum = 0;
 
-	if (n >= 0 && n <= 20)
+	for (test_num = 0; test_num <= 1024; test_num++)
 	{
-		for (row = 0; row <= n; row++)
+		if ((test_num % 3 == 0) || (test_num % 5 == 0))
 		{
-			for (column = 0; column <= n; column++)
-			{
-				int product = column * row;
-
-				if (column == 0)
-				{
-					putchar('0');
-				}
-				else if (product <= 9)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + product);
-				}
-				else if (product > 99)
-				{
-					putchar(',');
-					putchar(' ');
-					putchar('0' + (product / 100));
-					putchar('0' + ((product / 10) % 10));
-					putchar('0' + (product % 10));
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-					putchar(' ');
-					putchar('0' + (product / 10));
-					putchar('0' + (product % 10));
-				}
-			}
-			putchar('\n');
+			sum += test_num;
 		}
 	}
+	printf("%d\n", sum);
+	return (0);
 }
-
