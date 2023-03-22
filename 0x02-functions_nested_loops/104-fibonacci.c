eb93f19a903cfb85c54533bf7083339327e9349e
RichardKungu <rkungu12@gmail.com>
2023-03-22 12:59:13 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
-#include <math.h>
 
 /**
- * main - entry point
- * Description: fibonacii sequence
- * Return: 0
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int counter = 2;
-	float num1 = 1;
-	float num2 = num1 + 1;
-	float num3 = num1 + num2;
+	int c, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
 
-	printf("%.0f, ", num1);
-	printf("%.0f, ", num2);
-
-	while (counter < 98)
+	n1 = 1;
+	n2 = 2;
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (c = 0; c < 96; c++)
 	{
-		counter++;
-		printf("%.0f", num3);
-		num1 = num2;
-		num2 = num3;
-		num3 = num1 + num2;
-
-		if (counter < 98)
+		if (boolean)
 		{
-			printf(", ");
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
 		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
 	}
 	printf("\n");
 	return (0);
